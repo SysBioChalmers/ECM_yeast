7fec56bb4c9610bc1d94baec3ba916904d771def
Ivan Domenzain <ivand@chalmers.se>
2018-12-06 11:52:22 +0100

    style: pipeline cleanup

--- enhanceReducedGEM.m
+++ enhanceReducedGEM.m
@@ -1,49 +1,38 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% [model_data,kcats,KMs] = enhanceReducedGEM(model,org_name,KEGGcode)
+function ECM_model =  enhanceReducedGEM
 %
-% Ivan Domenzain.   Last edited: 2018-10-09
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%function [model_data,kcats,KMs] = enhanceGEM(model,org_name,org_code)
- current       = pwd; 
- %Specify the GECKO toolbox directory
- repoPath_terminal = '/Users/ivand/Documents/GitHub/ECM_yeast';
- GECKO_path    = '/Users/ivand/Documents/GitHub/GECKO';
- yeast8_path   = '/Users/ivand/Documents/GitHub/yeast-GEM/ModelFiles/xml';
- org_name      = 'saccharomyces cerevisiae';
- org_code      = 'sce';
- %%%%%%%%%%%%%%%%%%%%%%% Preprocess reduced GEM  %%%%%%%%%%%%%%%%%%%%%%%%%%
- %Commented because the missing genes were also manually added to the model
- cd (yeast8_path)
- yeast8 = readCbModel('yeastGEM.xml');
- cd ([current '/Models'])
- model = importExcelModel('model.xlsx');
- cd ..
- model = createKEGGmodel(model,yeast8);
- %model = correctMetNames(model);
- cd ../Model_curation_scripts
- model = addNewFields(model,yeast8);
- % Get EC numbers for each reaction based on the grRules field
- cd ([GECKO_path '/geckomat/get_enzyme_data'])
- model_data = getEnzymeCodes(model);
- cd ([current '/Models'])
- save('reducedModel.mat','model','model_data') 
- %Save SBML file
- exportModel(model,'reducedYeast.xml',true)
- %%%%%%%%%%%%%%%%%%%% Get parameters from BRENDA %%%%%%%%%%%%%%%%%%%%%%%%%%
- cd (current)
- %Get all the unique EC# matched to the model and then analyse the distribution
- %of KM and Kcat values reported for each of these. EC numbers with more
- %than one value and narrow distributions (log(median)/log(max/min)>= 1 are
- %showed in the cell 'NarrowDists'
- %[NarrowKcats,KcatStats] = BRENDA_analysis(model_data,'Kcat');
- %[NarrowKMs,KMStats]     = BRENDA_analysis(model_data,'KM');
- %Reversibility consistency check between the LB and rev fields in the model
- %inconsistencies          = reversibilityConsistencyCheck(model);
- [Ks, Kp] = getKineticData(model_data,org_name,'catalytic rate constant');
- %plotCDF(Ks,Kp,'Kcat distributions','Kcat [1/s]')
- [Ks, Kp] = getKineticData(model_data,org_name,'Michaelis constant');
- %plotCDF(Ks,Kp,'KM substrates','KM [mM]')
+% Ivan Domenzain.   Last edited: 2018-12-04
 
+current       = pwd;
+%Specify the GECKO toolbox directory
+repoPath_terminal = '/Users/ivand/Documents/GitHub/ECM_yeast';
+GECKO_path    = '/Users/ivand/Documents/GitHub/GECKO';
+yeast8_path   = '/Users/ivand/Documents/GitHub/yeast-GEM/ModelFiles/xml';
+org_name      = 'saccharomyces cerevisiae';
+org_code      = 'sce';
+%%%%%%%%%%%%%%%%%%%%%%% Preprocess reduced GEM  %%%%%%%%%%%%%%%%%%%%%%%%%%
+cd (yeast8_path)
+yeast8 = readCbModel('yeastGEM.xml');
+cd ([current '/Models'])
+model = importExcelModel('model.xlsx');
+cd ..
+model = createKEGGmodel(model,yeast8);
+cd ../Model_curation_scripts
+model = addNewFields(model,yeast8);
+cd ([current '/Models'])
+save('reducedModel.mat','model')
+%Save SBML file
+exportModel(model,'reducedYeast.xml',true)
+%%%%%%%%%%%%%%%%%%%% Get parameters from BRENDA %%%%%%%%%%%%%%%%%%%%%%%%%%
+% Get EC numbers for each reaction based on the grRules field
+cd ([GECKO_path '/geckomat/get_enzyme_data'])
+model_data = getEnzymeCodes(model);
+cd (current)
+%Get Kcats (forward and backward) for each reaction based on EC#
+[Ks, Kp] = getKineticData(model_data,org_name,'catalytic rate constant');
+plotCDF(Ks,Kp,'Kcat distributions','Kcat [1/s]')
+%Get KM's (forward and backward) for each reaction based on EC#
+[Ks, Kp] = getKineticData(model_data,org_name,'Michaelis constant');
+plotCDF(Ks,Kp,'KM substrates','KM [mM]')
 %%%%%%%%%%%%%%%%%%%% Get Keq from Equilibrator  %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Use equilibrator (python) for determining Keq's for all of the enzymatic
 %reactions
@@ -57,5 +46,5 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%% Get ECM model structure %%%%%%%%%%%%%%%%%%%%%%%%%%
 ECM_model = getECM_model(model);
 cd (current)
-%end
+end
 
