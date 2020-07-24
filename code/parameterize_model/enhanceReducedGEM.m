d17e7e86a144cf6fe787b85377c89f5552b8076d
Ivan Domenzain <ivand@chalmers.se>
2020-07-24 20:29:12 +0200

    fix: fix directories issues

--- code/parameterize_model/enhanceReducedGEM.m
+++ code/parameterize_model/enhanceReducedGEM.m
@@ -1,26 +1,25 @@
 function ECM_model =  enhanceReducedGEM
+%enhanceReducedGEM
 %
 % Ivan Domenzain.   Last edited: 2019-10-11
 
-current       = pwd;
+current = pwd;
 %Specify the GECKO toolbox directory
 repoPath_terminal = '/Users/ivand/Documents/GitHub/ECM_yeast';
 GECKO_path    = '/Users/ivand/Documents/GitHub/GECKO';
-yeast8_path   = '/Users/ivand/Documents/GitHub/yeast-GEM/ModelFiles/xml';
+yeast8_path   = '/Users/ivand/Documents/GitHub/yeast-GEM/ModelFiles/mat';
 org_name      = 'saccharomyces cerevisiae';
 org_code      = 'sce';
 %%%%%%%%%%%%%%%%%%%%%%% Preprocess reduced GEM  %%%%%%%%%%%%%%%%%%%%%%%%%%
-model = open('../../models/model.mat');
-yeast8 = load('../../../yeast-GEM/ModelFiles/mat/yeastGEM.mat');
+model = load('../../models/model.mat');
+model = model.model;
+yeast8 = load([yeast8_path '/yeastGEM.mat']);
 yeast8 = yeast8.model;
-cd ..
 model = createKEGGmodel(model,yeast8);
-cd ../Model_curation_scripts
 model = addNewFields(model,yeast8);
-cd ([current '/Models'])
-save('reducedModel.mat','model')
+save('../../models/reducedModel.mat','model')
 %Save SBML file
-exportModel(model,'reducedYeast.xml',true)
+exportModel(model,'../../models/reducedYeast.xml',true)
 %%%%%%%%%%%%%%%%%%%% Get parameters from BRENDA %%%%%%%%%%%%%%%%%%%%%%%%%%
 % Get EC numbers for each reaction based on the grRules field
 cd ([GECKO_path '/geckomat/get_enzyme_data'])
@@ -28,14 +27,18 @@ model_data = getEnzymeCodes(model);
 cd (current)
 %Get Kcats (forward and backward) for each reaction based on EC#
 [Ks, Kp] = getKineticData(model_data,org_name,'catalytic rate constant');
+cd ../visualization
 plotCDF(Ks,Kp,'Kcat distributions','Kcat [1/s]')
 %Get KM's (forward and backward) for each reaction based on EC#
+cd ../parameterize_model
 [Ks, Kp] = getKineticData(model_data,org_name,'Michaelis constant');
+cd ../visualization
 plotCDF(Ks,Kp,'KM substrates','KM [mM]')
 %%%%%%%%%%%%%%%%%%%% Get Keq from Equilibrator  %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Use equilibrator (python) for determining Keq's for all of the enzymatic
 %reactions
-commandStr = ['python ' repoPath_terminal '/getKeq.py'];
+cd ../parameterize_model
+commandStr = 'python getKeq.py';
 status = system(commandStr);
 if status~=0
     disp('getKeq.py failed to execute from MATLAB, try to run it separately')
